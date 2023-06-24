e480d74decaf5ffae109ffee49e91e1d16142988
Denis Dzyubenko <denis@ddenis.info>
2023-06-24 21:28:54 +0200

    Fixed opening Lemmy posts in Safari

--- Spud/Services/DataStore/Models/LemmyPost+localLemmyUiUrl.swift
+++ Spud/Services/DataStore/Models/LemmyPost+localLemmyUiUrl.swift
@@ -10,6 +10,12 @@ extension LemmyPost {
     /// Returns a link to this post, a url that can be opened in a browser.
     /// - Note: This assumes the instance runs standard install of Lemmy-UI which is not entirely correct.
     var localLemmyUiUrl: URL {
-        URL(string: "\(account.site.normalizedInstanceUrl)/post/\(localPostId)")!
+        let url = URL(string: account.site.normalizedInstanceUrl)!
+
+        if #available(iOS 16.0, *) {
+            return url.appending(path: "post/\(localPostId)")
+        } else {
+            return url.appendingPathComponent("post/\(localPostId)")
+        }
     }
 }
