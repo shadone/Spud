6ba382c7c3f2891c06edc498eb442ba3e8e794da
Denis Dzyubenko <denis@ddenis.info>
2023-07-12 21:32:43 +0200

    AccountList: Display nickname for signed in accounts

--- Spud/Scenes/Account/AccountList/AccountListAccountViewModel.swift
+++ Spud/Scenes/Account/AccountList/AccountListAccountViewModel.swift
@@ -11,29 +11,43 @@ class AccountListAccountViewModel {
     // MARK: Public
 
     var title: AnyPublisher<AttributedString, Never> {
-        site
-            .flatMap { site in
-                site.publisher(for: \.normalizedInstanceUrl)
-            }
-            .map { normalizedInstanceUrl in
-                URL(string: normalizedInstanceUrl)?.safeHost ?? ""
-            }
-            .combineLatest(titleAttributes)
-            .map { description, attributes in
-                AttributedString(description, attributes: .init(attributes))
+        normalizedInstanceHost
+            .combineLatest(nickname, titleAttributes)
+            .map { tuple in
+                let description = tuple.0
+                let nickname = tuple.1
+                let attributes = tuple.2
+
+                guard let nickname else {
+                    return AttributedString(description, attributes: .init(attributes))
+                }
+
+                return AttributedString("\(nickname)@\(description)", attributes: .init(attributes))
             }
             .eraseToAnyPublisher()
     }
 
     var subtitle: AnyPublisher<NSAttributedString?, Never> {
         account.publisher(for: \.isSignedOutAccountType)
-            .combineLatest(subtitleAttributes)
-            .map { isSignedOutAccountType, attributes in
-                guard isSignedOutAccountType else {
+            .combineLatest(subtitleAttributes, email)
+            .map { tuple in
+                let isSignedOutAccountType = tuple.0
+                let attributes = tuple.1
+                let email = tuple.2
+
+                guard !isSignedOutAccountType else {
+                    return NSAttributedString(
+                        string: "signed out (anonymous browsing)",
+                        attributes: attributes
+                    )
+                }
+
+                guard let email else {
                     return nil
                 }
+
                 return NSAttributedString(
-                    string: "signed out (anonymous browsing)",
+                    string: email,
                     attributes: attributes
                 )
             }
@@ -76,6 +90,77 @@ class AccountListAccountViewModel {
 
     private var site: AnyPublisher<LemmySite, Never> {
         account.publisher(for: \.site)
+            .eraseToAnyPublisher()
+    }
+
+    private var normalizedInstanceUrl: AnyPublisher<String, Never> {
+        site
+            .flatMap { site in
+                site.publisher(for: \.normalizedInstanceUrl)
+            }
+            .eraseToAnyPublisher()
+    }
+
+    private var normalizedInstanceHost: AnyPublisher<String, Never> {
+        normalizedInstanceUrl
+            .map { normalizedInstanceUrl in
+                URL(string: normalizedInstanceUrl)?.safeHost ?? ""
+            }
+            .eraseToAnyPublisher()
+    }
+
+    private var accountInfo: AnyPublisher<LemmyAccountInfo?, Never> {
+        account.publisher(for: \.accountInfo)
+            .eraseToAnyPublisher()
+    }
+
+    private var email: AnyPublisher<String?, Never> {
+        accountInfo
+            .flatMap { accountInfo -> AnyPublisher<String?, Never> in
+                guard let accountInfo else {
+                    return .just(nil)
+                }
+                return accountInfo.publisher(for: \.email)
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+
+    private var person: AnyPublisher<LemmyPerson?, Never> {
+        accountInfo
+            .flatMap { accountInfo -> AnyPublisher<LemmyPerson?, Never> in
+                guard let accountInfo else {
+                    return .just(nil)
+                }
+                return accountInfo.publisher(for: \.person)
+                    .wrapInOptional()
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+
+    private var personInfo: AnyPublisher<LemmyPersonInfo?, Never> {
+        person
+            .flatMap { person -> AnyPublisher<LemmyPersonInfo?, Never> in
+                guard let person else {
+                    return .just(nil)
+                }
+                return person.publisher(for: \.personInfo)
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+
+    private var nickname: AnyPublisher<String?, Never> {
+        personInfo
+            .flatMap { personInfo -> AnyPublisher<String?, Never> in
+                guard let personInfo else {
+                    return .just(nil)
+                }
+                return personInfo.publisher(for: \.name)
+                    .wrapInOptional()
+                    .eraseToAnyPublisher()
+            }
             .eraseToAnyPublisher()
     }
 
