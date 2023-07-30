195965e8f312ef8458f4ad992f823d6dafc71c14
Denis Dzyubenko <denis@ddenis.info>
2023-07-30 22:31:13 +0200

    Extracted VoteStatus.Action to separate file

--- Spud/Utils/VoteStatus.swift
+++ Spud/Utils/VoteStatus.swift
@@ -5,8 +5,6 @@
 //
 
 import Foundation
-import LemmyKit
-import os.log
 
 enum VoteStatus {
     /// Upvoted.
@@ -48,51 +46,6 @@ enum VoteStatus {
             return NSNumber(booleanLiteral: false)
         case .neutral:
             return nil
-        }
-    }
-}
-
-extension VoteStatus {
-    enum Action: CustomStringConvertible {
-        case upvote
-        case downvote
-
-        var description: String {
-            switch self {
-            case .upvote: return "upvote"
-            case .downvote: return "downvote"
-            }
-        }
-    }
-
-    /// The action to take if the user interacts with a vote button.
-    ///
-    /// E.g. if the post is already upvoted and the user pressed upvote button,
-    /// the effective action is to remote the vote aka "unvote".
-    func effectiveAction(for action: Action) -> ScoreAction {
-        switch (self, action) {
-        case (.up, .upvote),
-             (.down, .downvote):
-            return .unvote
-        case (_, .upvote):
-            return .upvote
-        case (_, .downvote):
-            return .downvote
-        }
-    }
-
-    func voteCountChange(for action: Action) -> Int64 {
-        switch (self, action) {
-        case (.neutral, .upvote),
-             (.down, .downvote):
-            return 1
-        case (.neutral, .downvote),
-             (.up, .upvote):
-            return -1
-        case (.down, .upvote):
-            return 2
-        case (.up, .downvote):
-            return -2
         }
     }
 }
