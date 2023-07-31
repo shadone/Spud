04a5a9a06bd42454a43e387a0581aa0c4ab09192
Denis Dzyubenko <denis@ddenis.info>
2023-07-31 23:58:45 +0200

    ImageService: replaced custom silly in-memory cache with NSCache
    One of the problems was that our cache implementation was not thread safe but
    was actually used from multiple threads. NSCache is thread-safe and more robust
    and handles memory warning signals. Awesome.

--- Spud/Services/Image/ImageService.swift
+++ Spud/Services/Image/ImageService.swift
@@ -48,9 +48,22 @@ protocol HasImageService {
 }
 
 class ImageService: ImageServiceType {
-    let memoryCache = ImageCache()
+    /// In-memory cache for loaded images.
+    ///
+    /// Each cache entry has associated cost that is the size of the image (width \* height)
+    let memoryCache: NSCache<NSURL, UIImage>
 
     let session = URLSession.shared
+
+    // MARK: Functions
+
+    init() {
+        memoryCache = NSCache()
+        // There is no science to this limit, only guesswork.
+        memoryCache.countLimit = 100
+        // Approx 1GB of memory assuming 1 byte per pixel.
+        memoryCache.totalCostLimit = 1024 * 1024 * 1024
+    }
 
     func fetch(
         _ url: URL,
@@ -58,7 +71,7 @@ class ImageService: ImageServiceType {
     ) -> AnyPublisher<ImageLoadingState, Never> {
         assert(Thread.isMainThread, "This code is not thread safe")
 
-        if let cachedImage = memoryCache.get(for: url) {
+        if let cachedImage = memoryCache.object(forKey: url as NSURL) {
             // no need to specify .receive(on:) here (neither RunLoop.main nor DispatchQueue.main).
             // Doing do will trigger the callbacks on the next runloop breaking UITableViewCell
             // configuration.
@@ -68,7 +81,9 @@ class ImageService: ImageServiceType {
 
         // TODO: check if the image is present in URLSession cache.
 
-        let cachedThumbnailImage = thumbnailUrl.flatMap { memoryCache.get(for: $0) }
+        let cachedThumbnailImage = thumbnailUrl.flatMap {
+            memoryCache.object(forKey: $0 as NSURL)
+        }
 
         return get(url)
             .map { image -> ImageLoadingState in
@@ -102,7 +117,11 @@ class ImageService: ImageServiceType {
                     return .fail(with: .cannotDecode)
                 }
 
-                self?.memoryCache.add(image, for: url)
+                self?.memoryCache.setObject(
+                    image,
+                    forKey: url as NSURL,
+                    cost: Int(image.size.width * image.size.height)
+                )
 
                 return .just(image)
             }
