b010f5a54afadfff4d3fbd5af8aa5bc053ff2c65
Denis Dzyubenko <denis@ddenis.info>
2023-08-18 20:43:43 +0200

    Moved launch performance unit test to separate test class
    It clashed with SBUITestTunnel that took longer and wasn't 100% reliable.

---
+++ SpudUITests/SpudLaunchPerformance.swift
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2023, Denis Dzyubenko <denis@ddenis.info>
+//
+// SPDX-License-Identifier: BSD-2-Clause
+//
+
+import XCTest
+
+class SpudLaunchPerformance: XCTestCase {
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
--- SpudUITests/SpudUITests.swift
+++ SpudUITests/SpudUITests.swift
@@ -71,13 +71,4 @@ class SpudUITests: XCTestCase {
         let secondCellSubtitle = secondCell.staticTexts["subtitle"].label
         XCTAssertTrue(secondCellSubtitle.contains("lemmyshitpost"))
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
