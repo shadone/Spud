5955dcbe0faf12899e88512bbfbc1edef673d5c8
Denis Dzyubenko <denis@ddenis.info>
2023-09-02 12:28:56 +0200

    Added Logger.assert function
    Asserts in debug builds and logs a warning message in production builds

---
+++ SpudUtilKit/Extensions/Logger+assert.swift
@@ -0,0 +1,25 @@
+//
+// Copyright (c) 2023, Denis Dzyubenko <denis@ddenis.info>
+//
+// SPDX-License-Identifier: BSD-2-Clause
+//
+
+import Foundation
+import OSLog
+
+extension Logger {
+    public func assert(
+        _ condition: @autoclosure () -> Bool,
+        _ message: @autoclosure @escaping () -> String,
+        file: StaticString = #fileID,
+        line: UInt = #line
+    ) {
+        if !condition() {
+            let message = message()
+            warning("Assertion failure at \(file, privacy: .public):\(line, privacy: .public): \(message, privacy: .public)")
+            #if DEBUG
+            Swift.assertionFailure(message, file: file, line: line)
+            #endif
+        }
+    }
+}
