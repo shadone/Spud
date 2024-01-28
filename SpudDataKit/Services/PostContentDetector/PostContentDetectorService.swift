c38379e64bb5d6a62310ec0bdbf786b8b188fe1d
Denis Dzyubenko <denis@ddenis.info>
2024-01-29 00:34:35 +0100

    PostContentDetector: only do offline checks
    All those online checks seems smart at the time but thinking again about it,
    not a good idea. It leaks information about the user making all those requests,
    this was not privacy friendly.

--- SpudDataKit/Services/PostContentDetector/PostContentDetectorService.swift
+++ SpudDataKit/Services/PostContentDetector/PostContentDetectorService.swift
@@ -21,11 +21,6 @@ public protocol HasPostContentDetectorService {
 }
 
 public class PostContentDetectorService: PostContentDetectorServiceType {
-    /// Local in-memory cache of the detected content types that involved network (async) calls.
-    /// This is to avoid doing too many network requests (and load remote servers) as well as improve user experience
-    /// when opening the same post requires the same work of detecting post content type.
-    var cache: [URL: PostContentType] = [:]
-
     public init() { }
 
     public func contentTypeForUrl(in postInfo: LemmyPostInfo) -> AnyPublisher<PostContentType, Never> {
@@ -36,14 +31,6 @@ public class PostContentDetectorService: PostContentDetectorServiceType {
         // TODO: we could do more offline checks here:
         // - check if the domain is in Core Data as LemmySite (i.e. link to pictrs resource).
         // - check if popular image hosting like imgur.
-
-        if let contentTypeFromCache = cache[url] {
-            return .just(contentTypeFromCache)
-        }
-
-        func addToCache(_ contentType: PostContentType) {
-            cache[url] = contentType
-        }
 
         func isImageMimeType(_ response: URLResponse) -> Bool {
             response.mimeType?.starts(with: "image/") ?? false
@@ -73,82 +60,6 @@ public class PostContentDetectorService: PostContentDetectorServiceType {
             return .just(image)
         }
 
-        let session = URLSession.shared
-
-        var request = URLRequest(url: url)
-        request.httpMethod = "HEAD"
-        return session.dataTaskPublisher(for: request)
-            .flatMap { (_: Data, response: URLResponse) -> AnyPublisher<PostContentType, URLError> in
-                guard let httpUrlResponse = response as? HTTPURLResponse else {
-                    fatalError("Not HTTP?")
-                }
-
-                switch httpUrlResponse.statusCode {
-                case 200..<299: // Success
-                    if isImageMimeType(response) {
-                        addToCache(image)
-                        return .just(image)
-                    }
-
-                    addToCache(externalLink)
-                    return .just(externalLink)
-
-                case 405: // HTTP 405: Method Not Allowed
-                    logger.debug("The server does not allow HEAD requests for url \(url.absoluteString, privacy: .public)")
-
-                    // fallback to retry the same request but this time as a GET request.
-                    let request = URLRequest(url: url)
-                    return session.dataTaskPublisher(for: request)
-                        .flatMap { (_: Data, response: URLResponse) -> AnyPublisher<PostContentType, URLError> in
-                            guard let httpUrlResponse = response as? HTTPURLResponse else {
-                                fatalError("Not HTTP?")
-                            }
-
-                            switch httpUrlResponse.statusCode {
-                            case 200..<299: // Success
-                                if isImageMimeType(response) {
-                                    addToCache(image)
-                                    return .just(image)
-                                }
-
-                                addToCache(externalLink)
-                                return .just(externalLink)
-
-                            default:
-                                addToCache(externalLink)
-                                return .just(externalLink)
-                            }
-                        }
-                        .eraseToAnyPublisher()
-
-                default:
-                    logger.error("""
-                        Failed to detect content type. \
-                        Received HTTP status \(httpUrlResponse.statusCode, privacy: .public) \
-                        for url \(url.absoluteString, privacy: .public)
-                        """)
-                    // We could not detect the content type, lets assume this is an external link,
-                    // but no need to cache this response.
-                    return .just(externalLink)
-                }
-            }
-            .map { postContentType in
-                logger.debug("""
-                    Detected content type \(postContentType.debugDescription, privacy: .public) \
-                    for url \(url.absoluteString, privacy: .public)
-                    """)
-                return postContentType
-            }
-            .catch { urlError -> AnyPublisher<PostContentType, Never> in
-                logger.error("""
-                    An error occurred while trying to detect content type \
-                    for url \(url.absoluteString, privacy: .public): \
-                    \(urlError.localizedDescription, privacy: .public)
-                    """)
-
-                return .just(externalLink)
-            }
-            .receive(on: RunLoop.main)
-            .eraseToAnyPublisher()
+        return .just(externalLink)
     }
 }
